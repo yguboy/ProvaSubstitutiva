68f8718e2e2730b4fbf005daf34013fb1596c58e
yguboy <ygorespada14@gmail.com>
2023-12-08 11:41:34 -0300

    FINALIZADO

---
+++ ProvaIMC/Migrations/20231208143112_ClasseUsuario.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProvaIMC.Migrations
+{
+    public partial class ClasseUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Nascimento = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
--- ProvaIMC/Models/Usuario.cs
+++ ProvaIMC/Models/Usuario.cs
@@ -1,7 +1,7 @@
 ﻿namespace ProvaIMC.Models;
 public class Usuario
 {
-    public int UsuarioId {get; set;}
-    public string? Nome {get; set;}
-    public string? Nascimento {get; set;}
+    public int? UsuarioId {get; set;}
+    public string Nome {get; set;}
+    public string Nascimento {get; set;}
 }
