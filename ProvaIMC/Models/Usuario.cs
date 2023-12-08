0a9c7f8c5f33e891806ddd0c28cd32ff08b31f93
yguboy <ygorespada14@gmail.com>
2023-12-08 10:54:17 -0300

    socorro

--- ProvaIMC/Models/Usuario.cs
+++ ProvaIMC/Models/Usuario.cs
@@ -3,5 +3,5 @@ public class Usuario
 {
     public int UsuarioId {get; set;}
     public string? Nome {get; set;}
-    public int nascimento {get; set;}
+    public string? Nascimento {get; set;}
 }
